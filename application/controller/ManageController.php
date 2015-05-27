6d690de479d93b9088860ece0a98aa15f9240725
Vo Ngoc Phuoc <phuocvo1101@gmail.com>
2015-05-27 10:55:15 +0700

    no message

--- application/controller/ManageController.php
+++ application/controller/ManageController.php
@@ -14,28 +14,36 @@ class ManageController extends  BaseController
 	
 	public function indexAction()
 	{
-       // $cu= $this->manageModel->getDistricts();
-        $Negatives = $this->manageModel->getNegatives();
+	   if(isset($_GET['search'])){
+	       $search=$_GET['search'];
+	   }else{
+	       $search='';
+	   }
+        
+	    if(isset($_POST['go'])){
+            $search= $_POST['search']?$_POST['search']:'';
+        }
+        
+        $Negatives = $this->manageModel->getNegatives($search);
         
         $limit = isset($_REQUEST['limit']) ?  $_REQUEST['limit'] : 3;
 
-        $Pagination = new Pagination($limit,'index.php?controller=negative&action=index');//,$base_url
-        $totalRecord = count($Negatives); // Tổng số user có trong database
-        $totalPages = $Pagination->totalPages($totalRecord); // Tổng số trang tìm được
-        $limit = (int)$Pagination->limit; // Số record hiển thị trên một trang
-        $start = (int)$Pagination->start(); // Vị trí của record
+        $Pagination = new Pagination($limit,'index.php?controller=negative&action=index&search='.$search);//,$base_url
+        $totalRecord = count($Negatives); 
+        $totalPages = $Pagination->totalPages($totalRecord); 
+        $limit = (int)$Pagination->limit; 
+        $start = (int)$Pagination->start();
 
-        $Negativelimit = $this->manageModel->getNegativeslimit($start,$limit);
+        $Negativelimit = $this->manageModel->getNegativeslimit($start,$limit,$search);
        // echo '<pre>'.print_r($Negativelimit,true).'</pre>';die();
         $listPage= $Pagination->listPages($totalPages);
 
 
         $this->template->assign('negatives',$Negativelimit);
+        $this->template->assign('search',$search);
 
         $this->template->assign('listPage',$listPage);
 
-
-        $this->template->assign('listPage',$listPage);
 
 		return $this->template->fetch('manage/index.tpl');
 	}
